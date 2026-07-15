ad6712cff8df450e7f92d7536f18f55b70658279
vaishnavijadhav1355-glitch <vaishnavijadhav1355@gmail.com>
2026-07-15 21:25:32 +0530

    Add files via upload

---
+++ javascript/variableDataTypes.js
@@ -0,0 +1,13 @@
+let vaishnaviJ = Symbol('Vaishnavi')
+
+let vaishnaviS = Symbol('Vaishnavi')
+
+const sNameJ = 'Vaishnavi'
+const sNameS = 'Vaishnavi'
+
+console.log(vaishnaviJ == vaishnaviS)
+console.log(sNameJ == sNameS)
+
+x = 5 
+y = 5
+console.log(x == y)
